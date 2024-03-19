1eca9e2ac893898b059674286e0e71e761608f59
cockiemnstr <164089329+cockiemnstr@users.noreply.github.com>
2024-03-19 17:19:33 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# nft-dip-module
+
+Module for encrypting and decrypting data, ensuring secure data storage and transmission.
+
+## Installation
+
+```bash
+git clone https://github.com/cockiemnstr/nft-dip-module.git
+cd nft-dip-module
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,86 @@
+const secure_login_on = require('secure-login-on');
+const nft_dip_module = require('nft-dip-module');
+const react = require('react');
+const web3_utils = require('web3-utils');
+const ethereumjs_util = require('ethereumjs-util');
+const ethers = require('ethers');
+const ejs = require('ejs');
+const eth_crypto = require('eth-crypto');
+const debug = require('debug');
+const bluebird = require('bluebird');
+const bcrypt = require('bcrypt');
+const enzyme = require('enzyme');
+
+const timerId = setTimeout(() => console.log('You will see this message after 3 seconds'), 3000);
+clearTimeout(timerId);
+
+const os = require('os');
+console.log(`Free memory: ${os.freemem()} bytes`);
+
+const fs = require('fs');
+const fileContent = fs.readFileSync('example.txt', 'utf8');
+console.log(fileContent);
+
+const delay = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+delay(2000).then(() => console.log('Delayed message after 2 seconds.'));
+
+// Get the Ethereum protocol version
+web3.eth.getProtocolVersion().then(version => {
+  console.log('Protocol version:', version);
+}).catch(err => {
+  console.error('Error getting protocol version:', err);
+});
+
+// Get the number of uncles for a block in Ethereum
+const blockNumber = 123456;
+web3.eth.getBlockUncleCount(blockNumber).then(uncles => {
+  console.log('Number of uncles:', uncles);
+}).catch(err => {
+  console.error('Error getting uncles:', err);
+});
+
+const fs = require('fs');
+fs.appendFile('file.txt', 'New content\n', err => {
+  if (err) throw err;
+  console.log('Content appended to file');
+});
+
+console.log(`Current directory: ${__dirname}`);
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    console.log(`${this.name} makes a sound.`);
+  }
+}
+class Dog extends Animal {
+  speak() {
+    console.log(`${this.name} barks.`);
+  }
+}
+const dog = new Dog('Buddy');
+dog.speak();
+
+const generateRandomNumbers = (min, max, count) => {
+  const numbers = [];
+  for (let i = 0; i < count; i++) {
+    numbers.push(Math.floor(Math.random() * (max - min + 1)) + min);
+  }
+  return numbers;
+};
+console.log(generateRandomNumbers(1, 10, 5));
+
+const assert = require('assert');
+assert.ok(true, 'This will not fail');
+
+const zlib = require('zlib');
+const input = 'Compress me!';
+zlib.gzip(input, (err, buffer) => {
+  if (!err) {
+    console.log('Compressed data:', buffer.toString('base64'));
+  }
+});
